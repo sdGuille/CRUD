bf3fffd2866d14742cb52760b579765706c3143b
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-02 13:27:52 -0600

    Update views.py

--- library/views.py
+++ library/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Welcome to my CRUD application</h1>")
