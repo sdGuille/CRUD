5a131c23c8d9c8152c259804e5052969004cbcf9
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-02 13:30:39 -0600

    Update views.py

--- library/views.py
+++ library/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Welcome to my CRUD application</h1>")
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
