733eb69632a92ef5b6c5735daf586a0d93ed4334
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-03 14:56:06 -0600

    Update settings.py
    Postgres settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.posgrestql',
+        'NAME': 'library',
+        'USER': 'postgres',
+        'PASSWORD': 'guille17',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
