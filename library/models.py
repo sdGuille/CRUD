ef7fddb9b7b06f1b56c9ad154223cb83a7161d56
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-03 15:21:40 -0600

    Update models.py
    Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, null=False, blank=False,
+                             verbose_name='Title')
+    image = models.ImageField(upload_to='images/', null=True,
+                              verbose_name="Image")
+    description = models.TextField(verbose_name="Description", null=True)
+
+    def __str__(self):
+        return self.title
