e80e9d5efb4563db582ee33c17fb162b7d62fdf3
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-03-04 10:33:53 -0600

    settings to  show images

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,5 +113,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/images/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('books', views.books, name='books'),
     path('books/create', views.create, name="create"),
     path('books/update', views.update, name="update"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
